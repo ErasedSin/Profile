ef3688fe08691b87655ee000b1dabb641c420ce6
Gaio <gaiomoreira199@gmail.com>
2024-09-22 09:00:46 -0300

    Atualizações múltiplas

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
- 
+const music = document.getElementById('background-music');
+const toggleButton = document.getElementById('toggle-music');
+
+music.volume = 0.2; // Define o volume da música
+
+toggleButton.addEventListener('click', () => {
+    if (music.paused) {
+        music.play();
+        toggleButton.textContent = '🎵';
+    } else {
+        music.pause();
+        toggleButton.textContent = '🎵';
+    }
+});
+
+// Inicia a música não mutada ao carregar a página
+music.play().catch(error => {
+    console.log('Erro ao tentar tocar a música:', error);
+});
